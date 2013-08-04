85c2481423f0bf90d3de3e8564e811e1eaadb9d4
Byron Ruth <b@devel.io>
2013-08-04 19:10:56 -0400

    Remove deprecated utils module

--- restlib2/utils.py
+++
@@ -1,6 +0,0 @@
-import warnings
-from preserialize.serialize import serialize
-warnings.warn('The `serialize` method has been removed from restlib2 core ' \
-    'and uses the django-preserialize library instead. Replace any imports ' \
-    'of `restlib.utils.serialize` to `preserialize.serialize.serialize`.',
-    DeprecationWarning)
