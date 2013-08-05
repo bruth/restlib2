cdae2e2e35f97f413c1f071f49ff7d24f89cdcad
Byron Ruth <b@devel.io>
2013-08-04 21:40:38 -0400

    Improve test_suite to take args from the command line

--- test_suite.py
+++ test_suite.py
@@ -1,6 +1,15 @@
 import os
+import sys
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 from django.core import management
-management.call_command('test', 'tests')
+
+apps = sys.argv[1:]
+
+if not apps:
+    apps = [
+        'tests',
+    ]
+
+management.call_command('test', *apps)
